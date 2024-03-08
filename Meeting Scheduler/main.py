15e0c3db432a3f8ef082976431333be99414e08e
Annu Goyal <annugoyal@Annus-MacBook-Air.local>
2024-03-09 00:18:12 +0530

    Few design questions

--- Meeting Scheduler/main.py
+++ Meeting Scheduler/main.py
@@ -1,3 +1,14 @@
+
 class Meeting:
-    def __init__(self, start_time) -> None:
+    def __init__(self, start_time, end_time, meeting_room, users) -> None:
         self.start_time = start_time
+        self.end_time = end_time
+        self.meeting_room = meeting_room
+        self.user_lists = users
+
+class Calender:
+    def __init__(self, user_id, meetings):
+        self.user_id = user_id
+        self.meetings = meetings
+
+
